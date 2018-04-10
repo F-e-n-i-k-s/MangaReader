f3c41bd3edcac569c8b0b5b09d746bf2405630b0
Vlad <F-e-n-i-k-s.ru@mail.ru>
2018-04-10 19:46:04 +0300

    Adding admin site

--- achievement/models.py
+++ achievement/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Achievement(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    icon = models.CharField()
+    icon = models.CharField(max_length=250)
